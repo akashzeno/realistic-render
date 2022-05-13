a705525076ff8fb9457f7439f94e85fe526dd1f1
Akash Chowdhury <akashzeno@users.noreply.github.com>
2022-05-13 16:19:09 +0530

    Path bug fixed

--- main.js
+++ main.js
@@ -47,12 +47,12 @@ function updateAllMaterials() {
 
 // Environment Map
 const environmentMap = cubeTextureLoader.load([
-	"/public/textures/environmentMaps/0/px.jpg",
-	"/public/textures/environmentMaps/0/nx.jpg",
-	"/public/textures/environmentMaps/0/py.jpg",
-	"/public/textures/environmentMaps/0/ny.jpg",
-	"/public/textures/environmentMaps/0/pz.jpg",
-	"/public/textures/environmentMaps/0/nz.jpg",
+	"/textures/environmentMaps/0/px.jpg",
+	"/textures/environmentMaps/0/nx.jpg",
+	"/textures/environmentMaps/0/py.jpg",
+	"/textures/environmentMaps/0/ny.jpg",
+	"/textures/environmentMaps/0/pz.jpg",
+	"/textures/environmentMaps/0/nz.jpg",
 ]);
 environmentMap.encoding = THREE.sRGBEncoding;
 scene.background = environmentMap;
